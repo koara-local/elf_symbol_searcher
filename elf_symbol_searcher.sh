d35368da8345286d98d5014fa607a736d2e5c154
Kohei Arao <koara.local.jp@gmail.com>
2016-05-01 17:19:54 +0900

    add elf_symbol_searcher.sh for debug

---
+++ elf_symbol_searcher.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APP=$1
+SYMBOL=$2
+
+LIST=$(echo $APP; ldd $APP \
+    | grep -v -E 'ld-linux*.so' \
+    | grep -v 'linux-vdso.so' \
+    | grep -v 'linux-gate.so' \
+    | awk '{print $3}')
+
+for f in $LIST
+do
+    echo $f
+    readelf -s -W $f | grep $SYMBOL
+done
